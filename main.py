526bd1c374d87509a60d4032051d1b808c92f50a
NikhilRautela <153644681+NikhilRautela@users.noreply.github.com>
2025-05-08 14:22:31 +0530

    Main file

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from visualizer import VisualizerApp
+
+def main():
+    root = tk.Tk()
+    app = VisualizerApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
